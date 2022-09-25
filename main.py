2c545d40a114cdb4d46e9765455c436096214351
Khoa Le <kl58277@uga.edu>
2022-09-25 15:26:43 -0400

    initialize main

---
+++ main.py
@@ -0,0 +1,20 @@
+answer = int(input("1. Draw your own lines\n2. Use the given shapes\n"))
+if answer == 1:
+    lines = int(input("How many lines do you wish to draw? "))
+    coordinates = []
+    for i in range(lines):
+        coordinate = []
+        print(f'line {i}')
+        x0 = int(input("Enter x0: "))
+        y0 = int(input("Enter y0: "))
+        x1 = int(input("Enter x1: "))
+        y1 = int(input("Enter y1: "))
+        coordinate.append(x0)
+        coordinate.append(y0)
+        coordinate.append(x1)
+        coordinate.append(y1)
+        coordinates.append(coordinate)
+
+if answer == 2:
+    shape = int(input("1. Square\n2.Triangle\n3.Pentagon\n"))
+    
